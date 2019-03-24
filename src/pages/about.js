e06cb74b2c8d4726ad8a2faa7224ef8843a016e2
Sawyer Burnett <sawyerburnett@gmail.com>
2019-03-24 12:09:13 -0700

    pushing some stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,5 +3,51 @@ import React from "react";
 export default () => (
   <div>
     <p>Sawyer Burnett</p>
+    <ul>
+      <li>
+        <a
+          href="http://github.com/burnett2k"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://linkedin.com/in/sawyer-burnett-9176411b"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </li>
+    </ul>
+
+    <p>Recommended Reading:</p>
+    <ul>
+      <li>
+        {" "}
+        <a
+          href="https://www.goodreads.com/book/show/25744928-deep-work?from_search=true"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Deep Work
+        </a>{" "}
+        by Cal Newport
+      </li>
+      <li>
+        {" "}
+        <a
+          href="https://www.goodreads.com/book/show/27968891-site-reliability-engineering?from_search=true"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Site Reliability Engineering
+        </a>{" "}
+        by Google
+      </li>
+    </ul>
   </div>
 );
