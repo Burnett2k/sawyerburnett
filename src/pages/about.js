f8ded84b506241a673226cac2460694c757b71f8
Sawyer Burnett <sawyerburnett@gmail.com>
2019-03-26 19:18:14 -0700

    add more recommended reading

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,6 +48,61 @@ export default () => (
         </a>{" "}
         by Google
       </li>
+      <li>
+        {" "}
+        <a
+          href="https://www.goodreads.com/book/show/17255186-the-phoenix-project?ac=1&from_search=true"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          The Phoenix Project
+        </a>{" "}
+        by Gene Kim, Kevin Behr, George Spafford
+      </li>
+      <li>
+        {" "}
+        <a
+          href="https://www.goodreads.com/book/show/4099.The_Pragmatic_Programmer?ac=1&from_search=true"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          The Pragmatic Programmer
+        </a>{" "}
+        by Andy Hunt and Dave Thomas
+      </li>
+      <li>
+        {" "}
+        <a
+          href="https://www.goodreads.com/book/show/19288230-scrum"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Scrum: The Art of Doing Twice the Work in Half the Time
+        </a>{" "}
+        by Jeff Sutherland
+      </li>
+      <li>
+        {" "}
+        <a
+          href="https://www.goodreads.com/book/show/23158207-how-google-works"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          How Google Works
+        </a>{" "}
+        by Eric Schmidt
+      </li>
+      <li>
+        {" "}
+        <a
+          href="https://www.goodreads.com/book/show/24724602-flash-boys"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Flash Boys
+        </a>{" "}
+        by Michael Lewis
+      </li>
     </ul>
   </div>
 );
