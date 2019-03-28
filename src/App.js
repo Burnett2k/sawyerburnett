69143e67fa3877aa2e02f144f815b2ec9d7d482f
Sawyer Burnett <sawyerburnett@gmail.com>
2019-03-28 06:10:57 -0700

    add css baseline to app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Root, Routes } from "react-static";
 import { Link } from "@reach/router";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import "./app.css";
 
 function App() {
   return (
     <Root>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/projects">Projects</Link>
-      </nav>
-      <div className="content">
-        <Routes />
-      </div>
+      <CssBaseline>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/projects">Projects</Link>
+        </nav>
+        <div className="content">
+          <Routes />
+        </div>
+      </CssBaseline>
     </Root>
   );
 }
