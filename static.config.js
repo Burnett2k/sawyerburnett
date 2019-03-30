5c639f7d27021d557dd107cb605af18815ceb673
Sawyer Burnett <sawyerburnett@gmail.com>
2019-03-30 15:22:49 -0700

    delete unused files, upgrade react-static

--- static.config.js
+++ static.config.js
@@ -15,28 +15,6 @@ export default {
         }
       }
     ]
-  ]
+  ],
+  siteRoot: "https://sawyerburnett.com"
 };
-
-//import axios from "axios";
-//example code for how to grabs posts and dynamically create routes from them
-// getRoutes: async () => {
-//   // const { data: posts } = await axios.get(
-//   //   'https://jsonplaceholder.typicode.com/posts'
-//   // )
-//   return [
-//     {
-//       path: '/blog',
-//       getData: () => ({
-//         posts,
-//       }),
-//       children: posts.map(post => ({
-//         path: `/post/${post.id}`,
-//         component: 'src/containers/Post',
-//         getData: () => ({
-//           post,
-//         }),
-//       })),
-//     },
-//   ]
-// },
