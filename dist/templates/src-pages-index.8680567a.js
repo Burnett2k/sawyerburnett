7cf53e97ded4248b8f75451a965ace87f83395b0
Sawyer Burnett <sawyerburnett@gmail.com>
2019-04-03 06:19:12 -0700

    commit artifacts

---
+++ dist/templates/src-pages-index.8680567a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{133:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(76);t.default=Object(c.withSiteData)(function(){return r.a.createElement("div",{style:{textAlign:"center"}},r.a.createElement("h1",null,"Sawyer Burnett"))})}}]);
